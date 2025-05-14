8b2eeb36af1bda96fef0884a076de5e10aeed32b
Sachintha <98406068+Sachintha-Samarathunga@users.noreply.github.com>
2025-05-14 21:42:17 +0530

    update changes

--- src/test/java/OMS/Positive/User.java
+++ src/test/java/OMS/Positive/User.java
@@ -7,8 +7,8 @@ import org.testng.annotations.Test;
 import utils.BaseTest;
 import utils.ExtentReportManager;
 
+import java.awt.*;
 import java.io.IOException;
-import java.lang.reflect.Method;
 
 public class User extends BaseTest {
 
@@ -16,17 +16,27 @@ public class User extends BaseTest {
     int i=1;
 
     @BeforeMethod
-    public void setUp(Method method) throws IOException, InterruptedException {
+    public void setUp() throws IOException, InterruptedException {
 
-        setUpReport(method);
         loadUrl();
         webSteps.login();
         webSteps.click("SideMenu User Tab");
         webSteps.click("SideMenu UserList Tab");
     }
 
-    @Test(priority = 1, description = "TS001: Create User with Valid Credentials")
+    @Test(priority = 1)
     public void createUser() throws InterruptedException {
+
+        ExtentReportManager.startTest("User Functionality", "<b>Create User</b>");
+        ExtentReportManager.testSteps("<b><font color='blue'>Test Case : </font>TC01: Verify that the user can successfully add a user</b>");
+        ExtentReportManager.testSteps("<b><font color='blue'>Test Steps : </font></b>" +
+                "<br>Step 1- Login to the System" +
+                "<br>Step 2- Click User " +
+                "<br>Step 3- Click User List" +
+                "<br>Step 4- Click 'Add New User' Button" +
+                "<br>Step 5- Fill Primary Details" +
+                "<br>Step 6- Click 'Save' Button"
+        );
 
         this.emailAddress = webSteps.randomPersonalEmailAddress();
 
@@ -35,11 +45,12 @@ public class User extends BaseTest {
         webSteps.type("Amal", "Name Field");
         webSteps.click("Role Field");
         webSteps.selectFromDropdown();
-//        webSteps.type("712345678", "Contact Number Field");
+        webSteps.type("712345678", "Contact Number Field");
         webSteps.type(this.emailAddress, "Email Address Field");
         webSteps.type("AmalaX@%234", "Password Field");
         webSteps.type("AmalaX@%234", "Confirm Password Field");
         webSteps.type("NO: 523, Ja-ela road, Gampaha", "Address Field");
+        webSteps.click("Save Button");
 
         webSteps.waiting();
     }
@@ -47,17 +58,26 @@ public class User extends BaseTest {
     @DataProvider(name = "userSearchData")
     public Object[][] userSearchData() {
         return new Object[][]{
-                {"Name", "Kasun Bandara"},
+                {"Name", "Amal"},
                 {"Role", "Admin"},
                 {"Email", this.emailAddress},
                 {"Address", "Dewalegama,Kegalle"}
         };
     }
 
-    @Test(dataProvider = "userSearchData", priority = 2, description = "TS002: Search User with Valid Filters")
+    @Test(dataProvider = "userSearchData", priority = 2)
     public void searchUser(String type, String searchInput) throws InterruptedException {
         if(i==3){ i+=1;};
-        ExtentReportManager.testSteps("<b><font color='blue'>Test Case : </font>TC001: Search User with " + type + "</b>");
+        ExtentReportManager.startTest("User Functionality", "<b>Search Existing User by " + type + "</b>");
+        ExtentReportManager.testSteps("<b><font color='blue'>Test Case : </font>Verify that the user can search by " + type.toLowerCase() + "</b>");
+        ExtentReportManager.testSteps("<b><font color='blue'>Test Steps : </font></b>" +
+                "<br>Step 1 - Login to the System" +
+                "<br>Step 2 - Click User" +
+                "<br>Step 3 - Click User List" +
+                "<br>Step 4 - Select '" + type + "' from 'Search By' dropdown" +
+                "<br>Step 5 - Enter Search Input" +
+                "<br>Step 6 - Click Search"
+        );
 
         webSteps.passValue(type, "Search Dropdown");
         webSteps.type(searchInput, "Search Box");
@@ -67,23 +87,60 @@ public class User extends BaseTest {
     }
 
 
-    @Test(priority = 3, description = "TS003: Update Existing User from the List")
+    @Test(priority = 3)
     public void updateUser() throws InterruptedException {
+        ExtentReportManager.startTest("User Functionality", "<b>Update Existing User</b>");
+        ExtentReportManager.testSteps("<b><font color='blue'>Test Case : </font>TC03: Verify that the user can successfully edit a user</b>");
+        ExtentReportManager.testSteps("<b><font color='blue'>Test Steps : </font></b>" +
+                "<br>Step 1- Login to the System" +
+                "<br>Step 2- Click User " +
+                "<br>Step 3- Click User List" +
+                "<br>Step 4- Select a User" +
+                "<br>Step 5- Click Edit Action" +
+                "<br>Step 6- Edit Details" +
+                "<br>Step 7- Click 'Update' Button"
+        );
+
         if(webSteps.searchElement(1,4).getText().equals("kasun@gmail.com")){
-            webSteps.editAction(2).click();
+            webSteps.clickAction(2, 1).click();
         } else {
-            webSteps.editAction(1).click();
+            webSteps.clickAction(1, 1).click();
         }
 
         webSteps.elementToBeVisible("Name Field");
         webSteps.type(webSteps.getRandomNameFromTheList(), "Name Field");
         webSteps.click("Role Field");
         webSteps.selectFromDropdown();
-//        webSteps.type("712345678", "Contact Number Field");
+        webSteps.type("712345678", "Contact Number Field");
         webSteps.type(this.emailAddress, "Email Address Field");
         webSteps.type("NO: 200, Galle road, Matara", "Address Field");
 
         webSteps.waiting();
+    }
+
+    @Test(priority = 4)
+    public void changeUserPassword() throws InterruptedException, AWTException {
+        ExtentReportManager.startTest("User Functionality", "<b>Change User Password</b>");
+        ExtentReportManager.testSteps("<b><font color='blue'>Test Case : </font>TC04: Verify that the user can successfully change the user password</b>");
+        ExtentReportManager.testSteps("<b><font color='blue'>Test Steps : </font></b>" +
+                "<br>Step 1- Login to the System" +
+                "<br>Step 2- Click User " +
+                "<br>Step 3- Click User List" +
+                "<br>Step 4- Click 'Password Change' Action" +
+                "<br>Step 6- Enter New Password and Confirm Password" +
+                "<br>Step 7- Click 'Reset' Button"
+        );
+
+
+        if(webSteps.searchElement(1,4).getText().equals("kasun@gmail.com")){
+            webSteps.clickAction(2, 2).click();
+        } else {
+            webSteps.clickAction(1, 2).click();
+        }
+        webSteps.type("AmalaX@%234", "New Password Field");
+        webSteps.type("AmalaX@%234", "Confirm Password Field");
+        webSteps.click("Reset Button");
+        Assert.assertEquals("Password changed successfully",webSteps.getText("Toast Message"), "Passed");
     }
 
 }
