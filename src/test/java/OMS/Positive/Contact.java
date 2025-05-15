37b5ddf91afda784c4636a3a1ae4836cf0c72ca8
Sachintha <98406068+Sachintha-Samarathunga@users.noreply.github.com>
2025-05-15 14:37:13 +0530

    Implement Update Customer

--- src/test/java/OMS/Positive/Contact.java
+++ src/test/java/OMS/Positive/Contact.java
@@ -41,4 +41,29 @@ public class Contact extends BaseTest {
         //webSteps.click("AddContact_SaveButton");
         //Assert.assertEquals("Contact created successfully",webSteps.getText("ToastMessage"), "Passed");
     }
+
+    @Test(priority = 2)
+    public void updateContact() throws InterruptedException, AWTException {
+        ExtentReportManager.startTest("Contacts Functionality", "<b>Update Customer</b>");
+        ExtentReportManager.testSteps("<b><font color='blue'>Test Case : </font>TC01: Verify that the user can successfully update a customer</b>");
+        ExtentReportManager.testSteps("<b><font color='blue'>Test Steps : </font></b>" +
+                "<br>Step 1- Login to the System" +
+                "<br>Step 2- Navigated to the contacts" +
+                "<br>Step 3- Selected a Customer" +
+                "<br>Step 4- Clicked Edit Action" +
+                "<br>Step 5- Edit Details" +
+                "<br>Step 6- Clicked 'Update' Button"
+        );
+        webSteps.clickAction(1,1);
+        webSteps.type("kalani","Contacts-Contact Name Field");
+        webSteps.type("CU000102","Contacts-ReferenceID");
+        webSteps.type("761234568","Contacts-Contact No.1");
+        webSteps.type("767654321","Contacts-Contact No.2");
+        webSteps.type("250/A, Galle road, Panadura","Contacts-Shipping Address Field");
+        webSteps.click("Contacts-City Field");
+        webSteps.selectFromDropdown();
+        webSteps.type("contact10@gmail.com","Contacts-Email Field");
+        //webSteps.click("EditCustomer_UpdateButton");
+        //Assert.assertEquals("Contact created successfully",webSteps.getText("ToastMessage"), "Passed");
+    }
 }
