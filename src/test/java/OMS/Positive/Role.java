aa55f008da33fb85d8e949091bad571fbb8b5f58
Sachintha <98406068+Sachintha-Samarathunga@users.noreply.github.com>
2025-05-19 19:13:30 +0530

    Implement Update Role

--- src/test/java/OMS/Positive/Role.java
+++ src/test/java/OMS/Positive/Role.java
@@ -21,6 +21,12 @@ public class Role extends BaseTest {
         webSteps.click("SideMenu Roles Tab");
     }
 
+    public void search() throws InterruptedException {
+        webSteps.passValue("Role","Search Dropdown");
+        webSteps.type(PropertyUtils.getProperty("Role_Name"),"Search Box");
+        webSteps.click("Search Button");
+    }
+
     @Test(priority = 1)
     public void addRole() throws InterruptedException, AWTException {
         ExtentReportManager.startTest("Roles Functionality", "<b>Create Role</b>");
@@ -54,10 +60,29 @@ public class Role extends BaseTest {
                 "<br>Step 4 - Entered Search Input" +
                 "<br>Step 5 - Clicked Search"
         );
-        webSteps.passValue("Role","Search Dropdown");
-        webSteps.type(PropertyUtils.getProperty("Role_Name"),"Search Box");
-        webSteps.click("Search Button");
 
+        search();
         Assert.assertEquals(webSteps.searchElement(1,1).getText().trim(), PropertyUtils.getProperty("Role_Name").trim(), "Search result does not match input value.");
+    }
+
+    @Test(priority = 3)
+    public void editRole() throws InterruptedException, AWTException {
+        ExtentReportManager.startTest("Roles Functionality", "<b>Update Role</b>");
+        ExtentReportManager.testSteps("<b><font color='blue'>Test Case : </font>TC03: Verify that the user can successfully update a role</b>");
+        ExtentReportManager.testSteps("<b><font color='blue'>Test Steps : </font></b>" +
+                "<br>Step 1- Logged in to the System" +
+                "<br>Step 2- Clicked User" +
+                "<br>Step 3- Clicked Roles" +
+                "<br>Step 4 - Searched the role name that needs to be edited" +
+                "<br>Step 5 - Clicked Edit Action" +
+                "<br>Step 6 - Made the necessary changes" +
+                "<br>Step 7 - Clicked Update"
+        );
+        search();
+        webSteps.clickAction(1,1).click();
+        webSteps.type(webSteps.generateRandomRoleName(),"Role Name Field");
+        webSteps.click("Update Button");
+
+        Assert.assertEquals("Role updated successfully",webSteps.getText("ToastMessage"), "Passed");
     }
 }
